3790ef5331fb9061a18a75c429a3bc1fbb965174
Kartik Yadav <ky5986390@gmail.com>
2024-03-12 21:36:06 +0530

    till checkoutpage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import { Counter } from './features/counter/Counter';
 import './App.css';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 
-import { createRoot } from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   Link,
-} from "react-router-dom";
-
+} from 'react-router-dom';
+import Cart from './features/cart/Cart';
+import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout.js';
+// import ProductDetailPage from './pages/ProductDetailPage.js';
 const router = createBrowserRouter([
   {
     path: '/',
@@ -25,12 +27,24 @@ const router = createBrowserRouter([
     path: '/signup',
     element: <SignupPage></SignupPage>,
   },
+  { 
+    path: '/cart',
+    element: <CartPage></CartPage>,
+  },
+  { 
+    path: '/checkout',
+    element: <Checkout></Checkout>,
+  },
+  // { 
+  //   path: '/product-detail',
+  //   element: <ProductDetailPage></ProductDetailPage>,
+  // },
 ]);
 
 function App() {
   return (
     <div className="App">
-       <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </div>
   );
 }
