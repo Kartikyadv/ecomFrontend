c2f995accf8c06938562c28ba688bf47f8ac90d7
Kartik Yadav <ky5986390@gmail.com>
2024-03-16 20:33:40 +0530

    p5 sorting

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -6,6 +6,7 @@ import {
   ShoppingCartIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
 
 const user = {
   name: "Tom Cook",
@@ -37,6 +38,7 @@ const Navbar = ({ children }) => {
               <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="flex h-16 items-center justify-between">
                   <div className="flex items-center">
+                    <Link to="/">
                     <div className="flex-shrink-0">
                       <img
                         className="h-8 w-8"
@@ -44,6 +46,7 @@ const Navbar = ({ children }) => {
                         alt="Your Company"
                       />
                     </div>
+                    </Link>
                     <div className="hidden md:block">
                       <div className="ml-10 flex items-baseline space-x-4">
                         {navigation.map((item) => (
